2b48a8446dcd43c09124e0a40954715487f9f748
Toby2507 <98445016+Toby2507@users.noreply.github.com>
2022-06-20 22:59:30 +0100

    updated models.py

--- I4G001039OXK/blog/models.py
+++ I4G001039OXK/blog/models.py
@@ -7,6 +7,6 @@ from django.contrib.auth import get_user_model
 class Post(models.Model):
     Title = models.CharField(max_length=200)
     Text = models.TextField()
-    Author = get_user_model()
+    Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="blog_posts")
     Created_date = models.DateTimeField()
     Published_date = models.DateTimeField()
